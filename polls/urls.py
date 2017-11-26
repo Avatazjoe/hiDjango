9b239a42fad02d3f86668b7fb6dafac68553bfe9
U130709015 <anil.selvi13@gmail.com>
2017-11-26 22:23:34 +0300

    Main Template

--- polls/urls.py
+++ polls/urls.py
@@ -3,13 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^hello$',views.helloWorld),
-    url(r'^$',views.index, name='index'),
-    url(r'^(?P<question_id>[0-9]+)/$',  views.detail, name="detail"),
-    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name= "results"),
-    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name="vote")
-
-
-
-
-
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
